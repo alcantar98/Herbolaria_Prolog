33d25b5b453051210e9e448e4c5c213ca7dee34e
Jesus Alcantar <40283345+alcantar98@users.noreply.github.com>
2021-02-27 19:19:51 -0600

    creacion y subida del archivo
    relacion de conceptos de herbolaria

---
+++ Herbolaria.pl
@@ -0,0 +1,108 @@
+herbolaria(Plantas).
+
+imparte(Curanderos).
+imparte(Boticarios).
+
+plantas(Contiene).
+
+contiene(Vitaminas).
+contiene(Hormonas).
+contiene(Minerales).
+contiene(Metaloides).
+contiene(Proteinas).
+contiene(Enzimas).
+contiene(Alcaloides).
+contiene(Oligoelementos).
+
+planta(Abrojo).
+planta(Acacia).
+planta(Acanto).
+planta(Aceitilla).
+planta(Achicoria).
+planta(Acónito).
+planta(Adormidera).
+planta(Aguacate).
+planta(Ahuehuete).
+planta(Ajenjo).
+planta(Ajo).
+planta(Albahaca).
+planta(Alcachofa).
+planta(Alcanfor).
+planta(Amapola_Amarilla).
+planta(Amate).
+planta(Anís).
+planta(Anacahuite).
+planta(Ärnica).
+planta(Barbasco).
+planta(Belladona).
+planta(Beroo).
+planta(Boldo).
+planta(Borraja).
+planta(Bugambilia).
+planta(Brionia_Blanca).
+
+modo_de_empleo(Abrojo, té).
+modo_de_empleo(Acacia, oral).
+modo_de_empleo(Acanto, té, directo).
+modo_de_empleo(Aceitilla, tónico).
+modo_de_empleo(Achicoria, té).
+modo_de_empleo(Acónito, oral).
+modo_de_empleo(Adormidera, té).
+modo_de_empleo(Aguacate, oral).
+modo_de_empleo(Ahuehuete, té).
+modo_de_empleo(Ajenjo, té).
+modo_de_empleo(Ajo, oral).
+modo_de_empleo(Albahaca, té).
+modo_de_empleo(Alcachofa, oral, té).
+modo_de_empleo(Alcanfor, tónico).
+modo_de_empleo(Amapola_Amarilla, té).
+modo_de_empleo(Amate, oral).
+modo_de_empleo(Anís, té).
+modo_de_empleo(Anacahuite, té, infusión).
+modo_de_empleo(Árnica, tónico).
+modo_de_empleo(Barbasco, té, cataplasmas).
+modo_de_empleo(Belladona, oral, té).
+modo_de_empleo(Berro, oral).
+modo_de_empleo(Boldo, té).
+modo_de_empleo(Borraja, té, cataplasma).
+modo_de_empleo(Bugambilia, infusión_con_miel).
+modo_de_empleo(Brionia_Blanca, té, diurético).
+
+/* plantas del alumno Carlos Alberto Contreras Ventura */
+
+planta(Amapola_Amarilla).
+planta(Amate).
+planta(Anís).
+planta(Anacahuite).
+
+modo_de_empleo(Amapola_Amarilla, té).
+modo_de_empleo(Amate, oral).
+modo_de_empleo(Anís, té).
+modo_de_empleo(Anacahuite, té, infusión).
+
+usos(Amapola_Amarilla, dolores_intestinales, abdominales, uterinos, antidiarreico).
+usos(Amate, reumatismos, diviesos, botar_la_solitaria, cerrar_ombligo_del_bebe).
+usos(Anís, cólicos, desinfectante_de_boca, antiflatulento).
+usos(Anacahuite, tos, bronquitis, asma, catarros, tubercolosis, dolor_de_pecho, graganta_irritada).
+
+formas(Amapola_Amarilla, en_las_mañanas).
+lugar(Amapola_Amarilla, baja_california).
+contenido(Anacahuite, calcio, potasio).
+
+/* plantas del alumno jose jesus Alcantar Saavedra */
+
+planta(Acanto).
+planta(Aceitilla).
+planta(Achicoria).
+
+modo_de_empleo(Acanto, té, directo).
+modo_de_empleo(Aceitilla, tónico).
+modo_de_empleo(Achicoria, té).
+
+usos(Acanto, desinflamatorio, almorranas, dolores_de_orina, disentería, aperitivo, antivenenoso, emoliente).
+usos(Aceitilla, tónico_nervioso, cerebral, depresion_nerviosa).
+usos(Achicoria, diurético, depurativo, cólicos, estreñimiento, purifia_riñones, disminuye_la_bilis).
+
+temporada(Aceitilla, Sembradios, agosto, octubre).
+
+
